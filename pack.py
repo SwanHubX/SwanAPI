ddb83e4228c56f5e635a0c8f6e6631df9c0099a4
ZeYiLin <944270057@qq.com>
2023-07-06 16:58:59 +0800

    Encapsulate a profile class

--- pack.py
+++ pack.py
@@ -1,36 +1,16 @@
-from utils.yaml_process import get_yaml
-
-
-def gpu_yaml_process(gpu: str):
-    if isinstance(gpu, bool):
-        return gpu
-
-    if gpu.lower().strip() == "true":
-        gpu = True
-    elif gpu.lower().strip() == "false":
-        gpu = False
-    else:
-        raise "[Error] 'gpu' in swan.yaml is not 'true' or 'false'"
-    return gpu
-
+"""
+将用户定义的swan.yaml转换为Dockerfile
+"""
+from utils.yaml_process import SwanConfig
 
 # 获取本地yaml文件
-config = get_yaml("swan.yaml")
+config = SwanConfig("swan.yaml")
 
-# 拆分yaml文件的build和predict部分
-config_buid = config["build"]
-config_predict = config["predict"]
-
-# 获取build中的信息
-gpu = gpu_yaml_process(config_buid["gpu"])
-system_packages = config_buid["system_packages"]
-python_version = config_buid["python_version"]
-python_packages = config_buid["python_packages"]
 
 # 根据build中的信息，构建一个Dockerfile
 with open("Dockerfile", "w") as f:
     # if not gpu:
-    if python_version == "3.10":
+    if config.python_version == "3.10":
         f.write("FROM ubuntu:22.04")
 
     # 换源、apt更新
@@ -49,19 +29,20 @@ RUN apt-get install -y python3 curl && \
     pip3 install -U pip && \ 
     pip3 config set global.index-url http://mirrors.cloud.aliyuncs.com/pypi/simple/ && \ 
     pip3 config set global.trusted-host mirrors.cloud.aliyuncs.com 
-    
     """)
 
     # 安装apt包
     apt_packages = ""
-    for package in system_packages:
+    for package in config.system_packages:
         apt_packages += package + " "
     f.write("""
-RUN apt-get install -y --no-install-recommends {}\n""".format(apt_packages))
+RUN apt-get install -r requirements.txt \ 
+    apt-get install -y --no-install-recommends {}
+    """.format(apt_packages))
 
     # 安装python库
     pip_packages = ""
-    for package in python_packages:
+    for package in config.python_packages:
         pip_packages += package + " "
     f.write("""
 RUN pip3 install --no-cache-dir {}\n""".format(pip_packages))
@@ -74,10 +55,14 @@ RUN echo "==> Clean up..."  && \
 
     # 复制文件夹文件到镜像中
     f.write("""
-COPY . /app\n""")
+COPY . /app
+    """)
 
     # 设置工作目录
-    f.write("WORKDIR /app\n")
+    f.write("""
+WORKDIR /app
+    """)
 
     # 设置运行命令
-    f.write("CMD [\"uvicorn\",\"{}\"]\n".format(config_predict))
+    f.write("""
+CMD [\"uvicorn\",\"{}\"]""".format(config.config_predict))
--- utils/yaml_process.py
+++ utils/yaml_process.py
@@ -1,16 +1,38 @@
 import yaml
 
-
-def get_yaml(filename: str):
-    """
-    Read the YAML file to dict.
-    :param filename: yaml file path
-    :return: dict
-    """
-    with open(filename, 'r') as file:
-        data = yaml.load(file, Loader=yaml.FullLoader)
-    return data
+"""
+封装一个特殊的yaml处理类，将yaml中的信息以config格式做处理，并检查类型是否正确
+"""
 
 
-if __name__ == "__main__":
-    print(get_yaml("../swan.yaml"))
+class SwanConfig:
+    def __init__(self, filename):
+        self.config = self.yaml2dict(filename)
+
+        # 拆分yaml文件的build和predict部分
+        self.config_buid = self.config["build"]
+        # 获取build中的信息
+        self.gpu = self.bool_typecheck(self.config_buid["gpu"])
+        self.system_packages = self.config_buid["system_packages"]
+        self.python_version = self.config_buid["python_version"]
+        self.python_packages = self.config_buid["python_packages"]
+
+        # 获得predict重的信息
+        self.config_predict = self.config["predict"]
+
+    def bool_typecheck(self, input):
+        # 如果yaml中的gpu项不为bool值，则报错
+        if isinstance(input, bool):
+            return input
+        else:
+            raise TypeError("[Error] 'gpu' in swan.yaml is not bool")
+
+    def yaml2dict(self, filename: str):
+        """
+        Read the YAML file to dict.
+        :param filename: yaml file path
+        :return: dict
+        """
+        with open(filename, 'r') as file:
+            data = yaml.load(file, Loader=yaml.FullLoader)
+        return data
