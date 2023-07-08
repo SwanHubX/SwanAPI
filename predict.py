e7507fce569bbe8a2d270c4d12fcbc32b3e69d14
ZeYiLin <944270057@qq.com>
2023-07-08 13:10:02 +0800

    update more code check

--- predict.py
+++ predict.py
@@ -1,11 +1,8 @@
-"""
-以下是模拟用户写的predict.py
-"""
 import cv2
 from server import SwanInference
 
 
-def predict(text1, image):
+def predict(image):
     input_image = image
     result_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
     return "success", result_image
@@ -13,5 +10,5 @@ def predict(text1, image):
 
 if __name__ == "__main__":
     api = SwanInference()
-    api.parameters(predict, inputs=["text", "image"], outputs=["text", "image"])
+    api.parameters(predict, inputs=["image"], outputs=["text", "image"])
     api.launch()
--- server.py
+++ server.py
@@ -1,7 +1,5 @@
-"""
-像Gradio一样的输入输出定义方式？
-比如：[image, text]之类的
-"""
+import io
+
 from flask import Flask, request, make_response
 from typing import Any, Callable, Dict, List, Optional, Type, Union
 from builder.utils import check_elements_in_list
@@ -14,21 +12,34 @@ import json
 
 app = Flask("app")
 
-class BytesEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, bytes):
-            return obj.decode('utf-8')
-        return json.JSONEncoder.default(self, obj)
 
 def bytes_encoder(obj):
+    """
+    字节流编码
+    """
     if isinstance(obj, bytes):
         return obj.decode('utf-8')  # 将字节流转换为字符串
     raise TypeError("Object of type {} is not JSON serializable".format(type(obj)))
 
-class SwanInference():
+
+class SwanInference:
     def __init__(self):
-        self.mode = None
+        self.mode = "both"  # ["both", "input_only", "output_only"]
         self.io_types = ["text", "image"]
+        self.fn = None
+        self.inputs = None
+        self.outputs = None
+        self.signature = None
+        self.param_names = None
+        self.return_type = None
+
+    def type_reset(self, item):
+        """根据输入的类型字符串，替换为对应的类型变量"""
+        TYPES = {
+            "text": str,
+            "image": bytes
+        }
+        return TYPES[item]
 
     def parameters(self,
                    fn: Callable,
@@ -49,50 +60,61 @@ class SwanInference():
         # 如果输入、输出定义为None或空列表，报错
         if (inputs is None or inputs == []) and (outputs is None or outputs == []):
             raise ValueError("Must provide at least one of `inputs` or `outputs`")
+        # 如果输出为空，但是输入不为空
         elif outputs is None or outputs == []:
             outputs = []
             self.mode = "input_only"
+        # 如果输入为空，但是输出不为空
         elif inputs is None or inputs == []:
             inputs = []
             self.mode = "output_only"
 
+        # 类型检查: 输入、输出的类型是否为str或list, 如果类型是str，转换为list
         assert isinstance(inputs, (str, list))
         assert isinstance(outputs, (str, list))
-
-        # 如果类型是str，转换为list
         if not isinstance(inputs, list):
             inputs = [inputs]
         if not isinstance(outputs, list):
             outputs = [outputs]
 
         # 类型检查: 输入的类型是否在io_types中
-        assert check_elements_in_list(inputs, self.io_types) or inputs == []
-        assert check_elements_in_list(outputs, self.io_types) or outputs == []
+        if self.mode == "both":
+            assert check_elements_in_list(inputs, self.io_types)
+            assert check_elements_in_list(outputs, self.io_types)
+        elif self.mode == "input_only":
+            assert check_elements_in_list(inputs, self.io_types)
+        else:
+            assert check_elements_in_list(outputs, self.io_types)
 
-        self.inputs = inputs
+        # 类型转换: 将输入、输出的类型转换为对应的类型变量
+        self.inputs = [self.type_reset(input) for input in inputs]
         self.outputs = outputs
 
-        # 得到fn函数的参数名
+        # 得到fn函数的签名信息
         self.signature = inspect.signature(self.fn)
+
+        # 得到fn函数的参数名
         parameters = self.signature.parameters
         self.param_names = [param for param in parameters]
 
+        # 判断输入的数量是否与定义的一致
+        assert len(self.param_names) == len(self.inputs), ValueError("输入的数量与定义的不一致")
+
+        # 得到fn返回值的信息
         self.return_type = self.signature.return_annotation
 
     def prediction(self, **inputs):
-        """
-        传入的文本是字符串, 传入的图片是File(...)
-        """
-        #TODO: 对输入类型根据self.inputs_dict做判断
         inputs_keys = list(inputs.keys())
-        assert check_elements_in_list(inputs_keys, self.param_names)
-        for keys, values in inputs.items():
+        assert check_elements_in_list(inputs_keys, self.param_names)  # 判断请求输入的参数名是否与定义的一致
+        for iter, (keys, values) in enumerate(inputs.items()):
+            assert isinstance(values, self.inputs[iter]), "输入的类型与定义的不一致"
             if isinstance(values, bytes):
+                print(type(values))
                 inputs[keys] = cv2.imdecode(np.frombuffer(values, np.uint8), cv2.IMREAD_UNCHANGED)
             elif isinstance(values, str):
                 inputs[keys] = values
 
-        #TODO: 加个空判断
+        # TODO: 加个空判断
         result = self.fn(**inputs)
         # 判断返回值类型是否为元组
         if isinstance(result, tuple):
@@ -102,6 +124,7 @@ class SwanInference():
 
         assert num_variables == len(self.outputs), "输出的数量与定义的不一致"
 
+        # 如果输出的变量数量1
         if num_variables == 1:
             if self.outputs == ["image"]:
                 assert isinstance(result, np.ndarray)
@@ -110,6 +133,7 @@ class SwanInference():
             elif self.outputs == ["text"]:
                 assert isinstance(result, str)
             result_json = {"content": result}
+        # 如果输出的变量数量>=2
         else:
             result = list(result)
             result_json = {}
@@ -124,10 +148,11 @@ class SwanInference():
 
         return json.dumps(result_json, default=bytes_encoder)
 
-    def launch(self):
+    def launch(self, server_name="0.0.0.0", port=8000):
         """
         用户的在API端的输入假设是["text", "image"]
         """
+
         @app.route("/", methods=["POST", "GET"])
         def get_prediction():
             inputs = request.form.to_dict()
@@ -138,4 +163,4 @@ class SwanInference():
             inputs.update(file_data)
             return self.prediction(**inputs)
 
-        app.run(host="0.0.0.0", port=8000)
+        app.run(host=server_name, port=port)
