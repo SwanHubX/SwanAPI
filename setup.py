308a8c253591a83892674c9c3d5fcd48c91f94bd
ZeYiLin <944270057@qq.com>
2023-07-11 09:21:49 +0800

    update v0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='swanapi',
-    version='0.2.0',
+    version='0.2.1',
     author='ZeYiLin',
     author_email='zeyi.lin@swanhub.co',
     description='A low threshold, high performance, compatible with a variety of different scenarios of'
